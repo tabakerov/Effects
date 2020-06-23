5dee8b3bfb38873aaf644a40b300dc77b4174f7f
Dmitry Tabakerov <tabakerov@outlook.com>
2020-06-23 16:37:34 +0300

    Initial Commit

---
+++ Effects/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Effects
+//
+//  Created by Dima on 23/06/2020.
+//  Copyright © 2020 Dima. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
